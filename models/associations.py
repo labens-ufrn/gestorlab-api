adc780e20b77276562e5e756cfab012dfa7422dc
JFmaia.dev <jfmaia.dev@gmail.com>
2024-02-27 18:40:31 -0300

    feat: creating associations for projeto with usuario and laboratorio (#3)

--- models/associations.py
+++ models/associations.py
@@ -9,3 +9,21 @@ class UsuarioLaboratorioAssociation(SQLModel, table=True):
     )
     usuario_id: uuid.UUID = Field(foreign_key="usuario.id") 
     laboratorio_id: uuid.UUID = Field(foreign_key="laboratorio.id")
+
+class UsuarioProjetoAssociation(SQLModel, table=True):
+    id: uuid.UUID = Field(
+        default_factory= uuid.uuid4, 
+        primary_key=True,
+        nullable=False
+    )
+    usuario_id: uuid.UUID = Field(foreign_key="usuario.id") 
+    projeto_id: uuid.UUID = Field(foreign_key="projeto.id")
+
+class LaboratorioProjetoAssociation(SQLModel, table=True):
+    id: uuid.UUID = Field(
+        default_factory= uuid.uuid4, 
+        primary_key=True,
+        nullable=False
+    )
+    laboratorio_id: uuid.UUID = Field(foreign_key="laboratorio.id") 
+    projeto_id: uuid.UUID = Field(foreign_key="projeto.id")

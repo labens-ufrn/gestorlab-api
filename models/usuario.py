cec16cce6523f896f9300c6b80e1fec005346ddb
JFmaia <jfmaia.dev@gmail.com>
2024-03-17 08:37:02 -0300

    feat: Modifying user models using sqlalchemy (#14)

--- models/usuario.py
+++ models/usuario.py
@@ -1,27 +1,38 @@
 import uuid
 from datetime import datetime
 from typing import Optional, List
-from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy import Integer, String, Column, Boolean
+from sqlalchemy.orm import relationship
+from core.config import settings
 
-class Usuario(SQLModel, table=True):
-    __tablename__: str = 'usuario'
+class Usuario(settings.DBBaseModel):
+    __tablename__ = 'usuario'
 
-    id: uuid.UUID = Field(
+    id = Column(
+        autoincrement=True,
         default_factory=uuid.uuid4,
         primary_key=True,
-        index=True,
         nullable=False
     )
-    first_name: str = Field(nullable=False)
-    second_name: str = Field(nullable=False)
-    registration: int = Field(nullable=False)
-    email: str = Field(nullable=False)
-    tel: Optional[int] = Field(nullable=True)
+    first_name = Column(String(256),nullable=False)
+    second_name= Column(String(256),nullable=False)
+    registration= Column(Integer, nullable=False)
+    email = Column(String(256),index=True, nullable=False, unique=True)
+    tel = Column(Integer(11),nullable=True)
     # Definindo a relação com Laboratorio
-    laboratorios: List["LaboratorioUsuarioAssociation"] = Relationship(back_populates="usuario")
+    laboratorios= relationship(
+        "Laboratorio",
+        back_populates="coordenador",
+        lazy="joined"
+    )
     # Definindo a relação com Projeto
-    projetos: List["ProjetoUsuarioAssociation"] = Relationship(back_populates="usuario")
+    projetos = relationship(
+        "Projeto",
+        back_populates="autor",
+        uselist=True,
+        lazy="joined"
+    )
 
-    data_inicial: Optional[datetime] = Field(default=datetime.now, nullable=False)
-    data_up: Optional[datetime] = Field(default=datetime.now, nullable=False)
-    tag: int = Field(default=2, nullable=False)
+    data_inicial = Column(default=datetime.now, nullable=False)
+    data_up = Column(default=datetime.now, nullable=False)
+    tag: int = Column(Integer(1),default=1, nullable=False)

df21e6eaac83690f5c5c63344db12dbd58f9072f
JFmaia <jfmaia.dev@gmail.com>
2024-03-17 08:33:44 -0300

    feat: Modified file (#14)

--- create_tables.py
+++ create_tables.py
@@ -1,4 +1,4 @@
-from sqlmodel import SQLModel
+from core.config import settings
 from core.database import engine
 
 async def create_tables() -> None:
@@ -6,8 +6,8 @@ async def create_tables() -> None:
     print("Criando as tabelas no banco de dados")
     
     async with engine.begin() as conn:
-        await conn.run_sync(SQLModel.metadata.drop_all)
-        await conn.run_sync(SQLModel.metadata.create_all)
+        await conn.run_sync(settings.DBBaseModel.metadata.drop_all)
+        await conn.run_sync(settings.DBBaseModel.metadata.create_all)
     
     print('Tabelas criadas com sucesso...')
 

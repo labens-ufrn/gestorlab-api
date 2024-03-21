c01d0b94bfd3cdffd375ce776ab93697eb96a1ee
JFmaia <jfmaia.dev@gmail.com>
2024-03-21 13:09:24 -0300

    feat: Add new router (#14)

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from api.endpoints import usuario
+from api.endpoints import usuario, laboratorio
 
 api_router = APIRouter()
 
 api_router.include_router(usuario.router, prefix='/usuarios', tags=["usuarios"])
+api_router.include_router(laboratorio.router, prefix='/laboratorios' ,tags=["laboratorios"])

1cda4d87b5afb37e77bfc77d1e4f7400246268b9
JFmaia <jfmaia.dev@gmail.com>
2024-03-20 21:29:23 -0300

    feat: Adjust in schema of projeto (#14)

--- schemas/projeto_schema.py
+++ schemas/projeto_schema.py
@@ -1,12 +1,13 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 from datetime import datetime
-from usuario_schema import UsuarioSchemaBase
+from .usuario_schema import UsuarioSchemaBase
 
 class ProjetoSchema(BaseModel):
+    id: Optional[UUID4] = None 
     titulo: str
     descricao: str
-    autor_id= str 
-    data_inicial= datetime
-    data_up= datetime
-    autor = Optional[UsuarioSchemaBase]
+    autor_id: str 
+    data_inicial: datetime
+    data_up: datetime
+    autor: Optional[UsuarioSchemaBase]

1ab18f6cc35af3f0f5d7d1a8642d1ca42729285f
JFmaia <jfmaia.dev@gmail.com>
2024-03-20 21:27:40 -0300

    feat: Adjust in model laboratorio (#14)

--- models/laboratorio.py
+++ models/laboratorio.py
@@ -1,21 +1,18 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import String, Column
+from sqlalchemy import String, Column, ForeignKey
 from sqlalchemy.orm import relationship
 from core.config import settings
+from sqlalchemy.dialects.postgresql import UUID
 
 class Laboratorio(settings.DBBaseModel):
-    __tablename__='laboratorios'
-    id = Column(
-        autoincrement=True,
-        default_factory=uuid.uuid4,
-        primary_key=True,
-        nullable=False
-    )
-    coordenador_id = Column(String, foreign_key="usuario.id", nullable=False)
+    __tablename__ = 'laboratorios'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    coordenador_id = Column(UUID(as_uuid=True), ForeignKey("usuario.id"), nullable=False)
     nome = Column(String(256), nullable=False)
     descricao = Column(String(5000), nullable=True)
     email = Column(String(256), nullable=True)
-    data_inicial = Column(default=datetime.now, nullable=False)
-    data_up = Column(default=datetime.now, nullable=False)
+    data_inicial = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
+    data_up = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
     coordenador = relationship("Usuario", back_populates='laboratorios', lazy='joined')

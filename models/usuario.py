78106ba01a5c66d7c9243560200ad1cc5b0f74bb
JFmaia <jfmaia.dev@gmail.com>
2024-03-20 21:28:09 -0300

    feat: Adjust in model usuario (#14)

--- models/usuario.py
+++ models/usuario.py
@@ -1,39 +1,32 @@
 import uuid
 from datetime import datetime
 from typing import Optional, List
-from sqlalchemy import Integer, String, Column, Boolean
+from sqlalchemy import BigInteger, String, Column, Boolean
 from sqlalchemy.orm import relationship
 from core.config import settings
+from sqlalchemy.dialects.postgresql import UUID
 
 class Usuario(settings.DBBaseModel):
     __tablename__ = 'usuario'
 
-    id = Column(
-        autoincrement=True,
-        default_factory=uuid.uuid4,
-        primary_key=True,
-        nullable=False
-    )
-    primeiro_nome = Column(String(256),nullable=False)
-    segundo_nome= Column(String(256),nullable=False)
-    matricula= Column(Integer, nullable=False, unique=True)
-    email = Column(String(256),index=True, nullable=False)
-    tel = Column(Integer(11),nullable=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    primeiro_nome = Column(String(256), nullable=False)
+    segundo_nome = Column(String(256), nullable=False)
+    matricula = Column(BigInteger, nullable=False, unique=True)
+    email = Column(String(256), index=True, nullable=False)
+    tel = Column(BigInteger, nullable=True)
     senha = Column(String(256), nullable=False)
-    # Definindo a relação com Laboratorio
-    laboratorios= relationship(
+    laboratorios = relationship(
         "Laboratorio",
         back_populates="coordenador",
         lazy="joined"
     )
-    # Definindo a relação com Projeto
     projetos = relationship(
         "Projeto",
         back_populates="autor",
         uselist=True,
         lazy="joined"
     )
-
-    data_inicial = Column(default=datetime.now, nullable=False)
-    data_atualizacao = Column(default=datetime.now, nullable=False)
-    tag = Column(Integer(1),default=1, nullable=False)
+    data_inicial = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
+    data_atualizacao = Column(String(256), default=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'), nullable=False)
+    tag = Column(BigInteger, default=1, nullable=False)

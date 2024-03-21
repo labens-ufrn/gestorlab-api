81ae918ecc7a79133aa21dcf7a7c7917e19a1d51
JFmaia <jfmaia.dev@gmail.com>
2024-03-21 13:13:16 -0300

    feat: Adjust in schema based and create new schema (#14)

--- schemas/laboratorio_schema.py
+++ schemas/laboratorio_schema.py
@@ -1,14 +1,23 @@
-from typing import Optional
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr, UUID4
 from datetime import datetime
 from .usuario_schema import UsuarioSchemaBase
 
 class LaboratorioSchema(BaseModel):
     id: Optional[UUID4] = None 
-    coordenador_id: str
+    coordenador_id: Optional[UUID4] = None 
     nome: str
     descricao: str
     email: EmailStr
-    data_inicial: datetime
-    data_up: datetime
-    coordenador: Optional[UsuarioSchemaBase]
+    data_inicial: Optional[datetime] = None 
+    data_up: Optional[datetime] = None
+    membros: Optional[List[UsuarioSchemaBase]] = None
+
+    class Config:
+        from_attributes = True
+
+class LaboratorioSchemaCreate(BaseModel):
+    nome: str
+    descricao: str
+    email: EmailStr
+    lista_membros: Optional[List[UUID4]] = None
